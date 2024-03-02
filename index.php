c5c5e922ef9cdddef859a575e7a2c8f113180a38
Hafida 3412 <lounsy81@gmail.com>
2024-03-02 21:37:33 +0100

    POO_1 Objet Entreprise

---
+++ index.php
@@ -0,0 +1,18 @@
+<h1>POO Entreprise</h1>
+
+<?php
+
+//require "Entreprise.php";
+//require "Employe.php;
+
+spl_autoload_register(function ($class_name){
+    require 'classes/'. $class_name. '.php';
+});
+
+$elanFormation = new Entreprise("ELAN FORMATION", "1993-01-01", "14 rue du Rhône",
+ "67100", "STRASBOURG");
+ $stephane = new Employe("SMAIL", "Stéphane", "stephane@elan-formation.fr");
+
+ // echo $elanFormation;
+ echo $elanFormation->getInfos();
+ echo 
